063eb4ba400fddb9bfdbdab6757ec8a33f73a2d7
AlmantasK <almantusk@gmail.com>
2023-09-09 10:56:02 +0300

    start: https://leetcode.com/problems/remove-letter-to-equalize-frequency/

---
+++ Coding-Kata/PhoneNumbers/XSolutionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace PhoneNumbers
+{
+    public class XSolutionTests
+    {
+        public class MethodName
+        {
+            [Fact]
+            public void When_Returns()
+            {
+
+            }
+        }
+    }
+}
